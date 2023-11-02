759331ac295717a9efce245ec08267c5dd4385f1
Errol Vidad <errolvidad@gmail.com>
2023-11-02 21:56:58 +0000

    all locations dynamically rendered to page

--- js/home.js
+++ js/home.js
@@ -6,14 +6,32 @@ let stores = [
   {
     name: 'Seattle',
     hours: 'Hours Open: 6am-7pm',
-    phoneNumbers: 'Contact Info: 555-1212',
+    phoneNumbers: 'Contact Info: 123-456-7890',
     location: '2901 3rd Ave #300, Seattle, WA 98121',
   },
   {
     name: 'Tokyo',
     hours: 'Hours Open: 6am-7pm',
-    phoneNumbers:'Contact Info: 555-1212',
+    phoneNumbers:'Contact Info: 222-222-2222',
     location: '1 Chome-1-2 Oshiage, Sumida City, Tokyo 131-8634'
+  },
+  {
+    name: 'Dubai',
+    hours: 'Hours Open: 6am-7pm',
+    phoneNumbers:'Contact Info: 333-333-333',
+    location: '1 Sheikh Mohammed bin Rashid Blvd - Dubai'
+  },
+  {
+    name: 'Paris',
+    hours: 'Hours Open: 6am-7pm',
+    phoneNumbers:'Contact Info: 444-444-4444',
+    location: 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris'
+  },
+  {
+    name: 'Lima',
+    hours: 'Hours Open: 6am-7pm',
+    phoneNumbers:'Contact Info: 555-555-5555',
+    location: 'Ca. Gral. Borgono cuadra 8, Miraflores 15074'
   }
 ];
 
