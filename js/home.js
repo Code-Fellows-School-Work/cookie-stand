149973ba2f42d5463647090948fcfb03d93803cb
Errol Vidad <errolvidad@gmail.com>
2023-11-02 21:58:52 +0000

    removed zombie code

--- js/home.js
+++ js/home.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('hello world');
-
 let stores = [
   {
     name: 'Seattle',
@@ -35,50 +33,36 @@ let stores = [
   }
 ];
 
-// Function to render store details
+// renders store elements
 function renderStore(store) {
-  // Looks for any element with an id="locations", i.e.
-  // <section id="locations">
   let rootElement = document.getElementById('locations');
-
-  // Create a new, empty section for a store
   let storeSection = document.createElement('section');
   storeSection.classList.add('location');
   rootElement.appendChild(storeSection);
 
   let firstTitle = document.createElement('h2');
-  // Give it some content
   firstTitle.textContent = store.name;
-  // Add it to the page, as a child of rootElement
   storeSection.appendChild(firstTitle);
 
-  // Add an unordered list to show the store details...
   let storeDataList = document.createElement('ul');
   storeSection.appendChild(storeDataList);
 
-  // Render hours
   const hoursItem = document.createElement('li');
   hoursItem.textContent = 'Hours: ' + store.hours;
   storeDataList.appendChild(hoursItem);
 
-  // Put the phone numbers in the UL
-
   const phoneItem = document.createElement('li');
-  // Add the actual number, from the store object
   phoneItem.textContent = store.phoneNumbers;
-  // Append that to the UL
   storeDataList.appendChild(phoneItem);
 
-
-  // Render location details
   const locationItem = document.createElement('li');
   locationItem.textContent = 'Location: ' + store.location;
   storeDataList.appendChild(locationItem);
 }
 
-// Loop through the stores array and render each store
+// loops through array of stores
 for (const store of stores) {
   renderStore(store);
 }
 
-
+// used John's class demo and ChatGPT to generate code structure
